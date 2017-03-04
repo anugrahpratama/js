fd0da9a874fea91fada0049be90dface8ccef265
Anugrah Pratama <anugrahputrapratama024@gmail.com>
2017-03-04 19:21:05 +0800

    Update n2four.js

--- n2four.js
+++ n2four.js
@@ -23,8 +23,8 @@ var x=Math.floor((Math.random()*4)+ 1);
 var xxx=null;
 if(x=="1"){xxx="apk.html"}
 if(x=="2"){xxx="tampilan-desktop.html"}
-if(x=="3"){xxx="advantages-of-being-home-business-owner.html"}
-if(x=="4"){xxx="practice-environmental-qwareness-to-our.html"}a_to_ve[i].href="http://www.n2four.com/p/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="3"){xxx="nokia-e1.html"}
+if(x=="4"){xxx="asus-zenfone-c.html"}a_to_ve[i].href="http://www.n2four.com/p/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
